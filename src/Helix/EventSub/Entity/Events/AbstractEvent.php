325f40e4bd88d3187e4cfe459a47a1922f4eec11
Aaricdev <aaricdev@simply-stream.com>
2021-10-23 19:26:55 +0200

    feat: Add ID to events

--- src/Helix/EventSub/Entity/Events/AbstractEvent.php
+++ src/Helix/EventSub/Entity/Events/AbstractEvent.php
@@ -4,4 +4,24 @@ namespace SimplyStream\TwitchApiBundle\Helix\EventSub\Entity\Events;
 
 abstract class AbstractEvent implements EventInterface
 {
+    /**
+     * @var string
+     */
+    protected $id;
+
+    /**
+     * @return string
+     */
+    public function getId(): string
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId(string $id): void
+    {
+        $this->id = $id;
+    }
 }
